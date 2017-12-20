f29a8e69201e5c3afe895f0b6200ce95e0764ba3
Giancarlo Anemone <ganemone@uber.com>
2017-12-20 08:49:53 -0800

    Refactor renderer plugin into separate file

--- src/server-app.js
+++ src/server-app.js
@@ -1,10 +1,11 @@
 /* @flow */
 /* eslint-env node */
 import {compose} from './compose.js';
-import Timing, {now, TimingToken} from './timing';
+import Timing, {TimingToken} from './timing';
 import BaseApp from './base-app';
 import {withMiddleware} from './with-middleware';
-import {withDependencies} from './with-dependencies';
+import getRendererPlugin from './renderer';
+
 import createSSRPlugin from './ssr';
 
 export default function(): Class<FusionApp> {
@@ -30,23 +31,4 @@ export default function(): Class<FusionApp> {
       return this._app.callback();
     }
   };
-}
-
-function getRendererPlugin(render) {
-  return withDependencies({timing: TimingToken})(({timing}) => {
-    return withMiddleware(async function renderer(ctx, next) {
-      const timer = timing.from(ctx);
-      timer.downstream.resolve(now() - timer.start);
-
-      if (ctx.element) {
-        const renderStart = now();
-        ctx.rendered = await render(ctx.element);
-        timer.render.resolve(now() - renderStart);
-      }
-
-      const upstreamStart = now();
-      await next();
-      timer.upstream.resolve(now() - upstreamStart);
-    });
-  });
 }
