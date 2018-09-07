d45c76d93967f776480bd465768730ec8d7e29c0
Dung Truong <checkraiser11@gmail.com>
2018-09-07 13:11:15 +0700

    Revert base-app

--- src/base-app.js
+++ src/base-app.js
@@ -8,7 +8,7 @@
 
 import {createPlugin} from './create-plugin';
 import {createToken, TokenType, TokenImpl} from './create-token';
-import {ElementToken, RenderToken, SSRDeciderToken, StreamingToken} from './tokens';
+import {ElementToken, RenderToken, SSRDeciderToken} from './tokens';
 import {SSRDecider} from './plugins/streaming-ssr';
 
 import type {aliaser, cleanupFn, FusionPlugin, Token} from './types.js';
